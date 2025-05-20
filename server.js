bc0b8f3ca316fc28a980ee9e908b10cc47e10788
Popevlad <emmanuelnzeh894@gmail.com>
2025-05-20 23:36:39 +0100

    changes made to server.js, index.js, lesson1.js, addition of package files mongodb, dotenv, .env, db folder

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello Emmanuel!');
-});
-
 const port = 3000;
+
+app.use('/', require('./routes/index.js'));
 
 app.listen(process.env.port || port);
   console.log('Web Server is listening at port ' + (process.env.port || port));
