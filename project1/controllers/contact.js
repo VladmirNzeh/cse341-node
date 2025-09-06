0c35c4e34eff05fef7a0b35fb70e8f30330e5a69
Emmanuel Chidiebere Nzeh <vladmirkenneth@gmail.com>
2025-09-06 13:43:59 -0700

    updated ObjectId typo

--- project1/controllers/contact.js
+++ project1/controllers/contact.js
@@ -1,35 +1,43 @@
 const mongodb = require('../db/connect');
+const { ObjectId } = require('mongodb');
 
 const getAllContacts = async (req, res) => {
-    try {
-        const result = await mongodb.getDb().db('mongodbVSCodePlaygro').collection('contacts').find();
-        result.toArray().then((contacts) => {
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(contacts);
-        });
-    } catch (err) {
-      res.status(500).json({message: err.message});
-    }
+  try {
+    const result = await mongodb
+      .getDb()
+      .db('mongodbVSCodePlaygro')
+      .collection('contacts')
+      .find()
+      .toArray();
+
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const getSingleContact = async (req, res) => {
-    try {
-        const contactId = req.params.id;
-        const result = await mongodb.getDb().db('mongodbVSCodePlaygro').collection('contacts').findOne({_id:
-            new require('mongodb').objectId(contactId)});
+  try {
+    const contactId = req.params.id;
+    const result = await mongodb
+      .getDb()
+      .db('mongodbVSCodePlaygro')
+      .collection('contacts')
+      .findOne({ _id: new ObjectId(contactId) });
 
-            if (!result) {
-                return res.status(404).json({message: 'Contact not found'});
-            }
+    if (!result) {
+      return res.status(404).json({ message: 'Contact not found' });
+    }
 
-            res.setHeader('content-Type', 'application/json');
-            res.status(200).json(result);
-          } catch (err) {
-            res.status(500).json({message: err.message});
-          }
-        };
-    
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
 module.exports = {
-    getAllContacts,
-    getSingleContact
-}
+  getAllContacts,
+  getSingleContact,
+};
